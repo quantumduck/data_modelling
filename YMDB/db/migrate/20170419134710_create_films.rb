20392933ab127243f51eb30b91703d80444ccc5e
quantumduck <quantum.duck@hotmail.com>
2017-04-19 09:55:40 -0400

    models

---
+++ YMDB/db/migrate/20170419134710_create_films.rb
@@ -0,0 +1,12 @@
+class CreateFilms < ActiveRecord::Migration[5.0]
+  def change
+    create_table :films do |t|
+      t.string :name
+      t.integer :year
+      t.text :description
+      t.integer :director_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ YMDB/db/migrate/20170419135104_create_actors_films.rb
@@ -0,0 +1,6 @@
+class CreateActorsFilms < ActiveRecord::Migration[5.0]
+  def change
+    create_table :actors_films do |t|
+    end
+  end
+end
